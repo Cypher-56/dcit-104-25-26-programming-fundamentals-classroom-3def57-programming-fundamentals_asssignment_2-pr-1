d980bc42e979435af22dee70750dd3f9920e4acf
Cypher-56 <ckyadjapong001@st.ug.edu.gh>
2026-07-30 18:51:37 +0000

    Update assignment_01_prime_checker.cpp

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,30 @@
 #include <iostream>
 using namespace std;
 
+bool isPrime( int x) {
+    if (x < 2){
+        return false;
+    } 
+    for (int i = 2; i * i <= x; i++){
+        if(x % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int a;
+        cout << "Enter a number";
+        cin >> a;
+
+    if (isPrime(a)){
+        cout << a << "is a prime number"<< endl;
+    } else {
+        cout << a << "is not a prime number" << endl;
+    }
+    return 0;
+
+}
+
+
